d7f07e5667863335f3552c8f37af5cf489becbf0
DREAMWORLDVOID <yshliu0321@icloud.com>
2018-11-07 00:47:02 +0800

    Rewrite Chessboard.cs for more flexibility

--- Core/Game/ChessBoard.cs
+++ Core/Game/ChessBoard.cs
@@ -16,87 +16,71 @@ namespace Core.Game
     {
         public delegate void OnOperationEvent();
 
-        protected ChessType(string name, ChessTypeName type)
+        public abstract string Name { get; }
+        public abstract ChessTypeName Type { get; }
+
+        public void Process(Game game)
         {
-            Name = name;
-            Type = type;
+            BeforeChessProcessed?.Invoke();
+            DoProcess(game);
+            AfterChessProcessed?.Invoke();
         }
 
-        public ChessTypeName Type { get; }
-        public string Name { get; }
-
-        public virtual void Process(ChessBoard board)
+        protected virtual void DoProcess(Game game)
         {
-            OnOperation?.Invoke();
         }
 
-        public event OnOperationEvent OnOperation;
-
-        private delegate void Operation();
+        public event OnOperationEvent BeforeChessProcessed;
+        public event OnOperationEvent AfterChessProcessed;
     }
 
     public class ChessCommon : ChessType
     {
-        public ChessCommon() : base("Common", ChessTypeName.Common)
-        {
-        }
+        public override string Name => "Common";
+        public override ChessTypeName Type => ChessTypeName.Common;
     }
 
     public class ChessKey : ChessType
     {
-        public ChessKey() : base("Key", ChessTypeName.Key)
-        {
-        }
-
-        public override void Process(ChessBoard board)
-        {
-            base.Process(board);
-        }
+        public override string Name => "Key";
+        public override ChessTypeName Type => ChessTypeName.Key;
     }
 
     public class ChessFlip : ChessType
     {
-        public ChessFlip() : base("Flip", ChessTypeName.Flip)
-        {
-        }
+        public override string Name => "Flip";
+        public override ChessTypeName Type => ChessTypeName.Flip;
 
-        public override void Process(ChessBoard board)
+        protected override void DoProcess(Game game)
         {
-            base.Process(board);
-            var board1 = new ChessTypeName[board.SizeLeft * board.SizeRight];
-            for (var i = 0; i < board.SizeLeft; ++i)
-            for (var j = 0; j < board.SizeRight; ++j)
-                board1[j * board.SizeRight + i] = board.GetChess(i, j);
-            var tempSize = board.SizeRight;
-            board.SizeRight = board.SizeLeft;
-            board.SizeLeft = tempSize;
-            board.Board = board1;
+            var oldBoard = game.Board;
+            var newBoard = new ChessBoard(oldBoard.SizeRight, oldBoard.SizeLeft);
+            for (var i = 0; i < oldBoard.SizeLeft; ++i)
+            for (var j = 0; j < oldBoard.SizeRight; ++j)
+                newBoard[j, i] = oldBoard[i, j];
+            game.Board = newBoard;
         }
     }
 
     public class ChessAddCol : ChessType
     {
-        public ChessAddCol() : base("AddCol", ChessTypeName.AddCol)
-        {
-        }
+        public override string Name => "AddCol";
+        public override ChessTypeName Type => ChessTypeName.AddCol;
 
-        public override void Process(ChessBoard board)
+        protected override void DoProcess(Game game)
         {
-            base.Process(board);
-            board.ResizeBoard(board.SizeLeft + 1, board.SizeRight);
+            game.Board.ResizeDelta(1, 0);
         }
     }
 
     public class ChessDelCol : ChessType
     {
-        public ChessDelCol() : base("DelCol", ChessTypeName.DelCol)
-        {
-        }
+        public override string Name => "DelCol";
+        public override ChessTypeName Type => ChessTypeName.DelCol;
 
-        public override void Process(ChessBoard board)
+        protected override void DoProcess(Game game)
         {
-            base.Process(board);
-            board.ResizeBoard(board.SizeLeft - 1, board.SizeRight);
+            game.Board.ResizeDelta(-1, 0);
         }
     }
 
@@ -124,48 +108,46 @@ namespace Core.Game
         }
     }
 
+    public class Game
+    {
+        public ChessBoard Board { get; set; }
+    }
+
     public class ChessBoard
     {
-        public ChessTypeName[] Board;
+        private ChessTypeName[] _board;
 
-        public ChessBoard()
+        public ChessBoard() : this(3, 3)
         {
-            ResizeBoard(3, 3);
         }
 
-        public static ChessBoard Current { get; private set; }
-
-        public int SizeLeft { get; set; }
-
-        public int SizeRight { get; set; }
-
-        public void MakeCurrent()
+        public ChessBoard(int sizeLeft, int sizeRight)
         {
-            Current = this;
+            Resize(sizeLeft, sizeRight);
         }
 
-        private void ProcessExtraChess(ChessTypeName typeName)
+        public int SizeLeft { get; private set; }
+
+        public int SizeRight { get; private set; }
+
+        public ChessTypeName this[int left, int right]
         {
-            ChessTypeManager.Get(typeName).Process(this);
+            get => _board[left * SizeLeft + right];
+            set => _board[left * SizeLeft + right] = value;
         }
 
-        public ChessTypeName GetChess(int left, int right)
+        public void ResizeDelta(int deltaLeft, int deltaRight)
         {
-            return Board[left * SizeLeft + right];
+            Resize(SizeLeft + deltaLeft, SizeRight + deltaRight);
         }
 
-        public void SetChess(ChessTypeName chess, int left, int right)
-        {
-            Board[left * SizeLeft + right] = chess;
-        }
-
-        public void ResizeBoard(int newSizeLeft, int newSizeRight)
+        private void Resize(int newSizeLeft, int newSizeRight)
         {
             var board = new ChessTypeName[newSizeLeft * newSizeRight];
             for (var i = 0; i < Math.Min(SizeLeft, newSizeLeft); ++i)
             for (var j = 0; j < Math.Min(SizeRight, newSizeRight); ++j)
-                board[i * newSizeLeft + j] = GetChess(i, j);
-            Board = board;
+                board[i * newSizeLeft + j] = this[i, j];
+            _board = board;
             SizeLeft = newSizeLeft;
             SizeRight = newSizeRight;
         }
